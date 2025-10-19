0127b6d04307ab152eb3b5065372d17caa31aa17
lakshithaiam <lakshithaiam@gmil.com>
2025-10-19 11:43:16 +0530

    add files

--- index.php
+++ index.php
@@ -1,32 +1,21 @@
 <?php
-echo "Hello World from PHP!";
-
-
-
 echo "<h1>Hello World from PHP!</h1>";
+
 echo "<p>Server Hostname: " . gethostname() . "</p>";
-echo "<p>Server IP: " . $_SERVER['SERVER_ADDR'] . "</p>";
-echo "<p>Client IP: " . $_SERVER['REMOTE_ADDR'] . "</p>";
 
+$serverIp = $_SERVER['SERVER_ADDR'] ?? "Not Available";
+echo "<p>Server IP: " . $serverIp . "</p>";
 
+$clientIp = $_SERVER['REMOTE_ADDR'] ?? "Not Available";
+echo "<p>Client IP: " . $clientIp . "</p>";
 
-
+// Visitor counter
 $file = "counter.txt";
-
-// Initialize counter if file not exists
-if(!file_exists($file)){
+if (!file_exists($file)) {
     file_put_contents($file, "0");
 }
-
-// Read current count
 $count = (int) file_get_contents($file);
-
-// Increase count
 $count++;
-
-// Save new count
 file_put_contents($file, $count);
-
-echo "<h1>Hello World from PHP!</h1>";
 echo "<p>You are visitor number: $count</p>";
 ?>
